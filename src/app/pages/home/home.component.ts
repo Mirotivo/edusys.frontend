8a854a83267d5d7d82fb7d0261d6d590cada7376
ahmedMgouda <eng.gouda91@gmail.com>
2025-02-18 08:49:16 +1100

    fix courses card ui

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -46,8 +46,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     responsive: {
       0: { items: 1 },
       400: { items: 2 },
-      740: { items: 3 },
-      940: { items: 4 }
+      740: { items: 3 }
     },
     nav: false,
     autoHeight: false
